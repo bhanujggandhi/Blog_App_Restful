200c3b91e67ad91dfbaa3d8db0c7385a3c91c93f
bhanujggandhi <gandhibhanuj@gmail.com>
2020-04-12 00:19:38 +0530

    Update and Delete Added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var bodyParser = require('body-parser'),
+	methodOverride = require('method-override'),
 	mongoose = require('mongoose'),
 	express = require('express'),
 	app = express();
@@ -8,6 +9,7 @@ mongoose.connect('mongodb://localhost:27017/blog_app', { useNewUrlParser: true, 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(methodOverride('_method'));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -60,6 +62,36 @@ app.get('/blogs/:id', function(req, res) {
 			res.redirect('/blogs');
 		} else {
 			res.render('show', { blog: foundBlog });
+		}
+	});
+});
+
+app.get('/blogs/:id/edit', function(req, res) {
+	Blog.findById(req.params.id, function(err, foundBlog) {
+		if (err) {
+			res.redirect('/blogs');
+		} else {
+			res.render('edit', { blog: foundBlog });
+		}
+	});
+});
+
+app.put('/blogs/:id', function(req, res) {
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+		if (err) {
+			res.redirect('/blogs');
+		} else {
+			res.redirect('/blogs/' + req.params.id);
+		}
+	});
+});
+
+app.delete('/blogs/:id', function(req, res) {
+	Blog.findByIdAndRemove(req.params.id, function(err) {
+		if (err) {
+			res.redirect('/blogs');
+		} else {
+			res.redirect('/blogs');
 		}
 	});
 });
